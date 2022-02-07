c3da0b4d11ecb545e7565cd952d61feabe3a097f
Johnny <jsloachamin1@espe.edu.ec>
2022-02-06 20:29:28 -0500

    Add Class Maquinas

---
+++ III-PARCIAL/Proyecto/MACHINES4FUN/Maquina.h
@@ -0,0 +1,40 @@
+/***********************************************************************
+ * Module:  Maquina.h
+ * Author:  Alvaro Zuma, Johnny Loachamin
+ * Modified: Saturday, February 06, 2021 8:55:50 PM
+ * Purpose: Declaration of the class Maquina
+ ***********************************************************************/
+
+#if !defined(__BIBLIOTECA_LISTA_DOBLE2_Libro_h)
+#define __BIBLIOTECA_LISTA_DOBLE2_Libro_h
+
+
+class Maquina
+{
+public:
+   Maquina();
+   ~Maquina();
+   int getIdMaquina(void);
+   void setIdMaquina(int newIdMaquina);
+   std::string getNombreMaquina(void);
+   void setNombreMaquina(std::string newNombreMaquina);
+   Autor getOperador(void);
+   void setOperador(Operador newOperador);
+   Pais getPais(void);
+   void setPais(Pais newPais);
+   Fabrica getFabrica(void);
+   void setFabrica(Fabrica newFabrica);
+
+protected:
+private:
+   int idMaquina;
+   std::string nombreMaquina;
+   Operador operador;
+   Pais pais;
+   Fabrica fabrica;
+
+
+};
+
+#endif
+
