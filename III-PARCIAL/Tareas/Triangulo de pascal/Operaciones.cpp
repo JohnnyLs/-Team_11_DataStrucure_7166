375c8573fd3cb8584fb6865bd071b6feb3816248
alvarozumba2 <57681168+alvarozumba2@users.noreply.github.com>
2022-02-06 22:21:47 -0500

    Update Operaciones.cpp

--- III-PARCIAL/Tareas/Triangulo de pascal/Operaciones.cpp
+++ III-PARCIAL/Tareas/Triangulo de pascal/Operaciones.cpp
@@ -2,43 +2,16 @@
 #include<iostream>
 using namespace std;
 
-int Operaciones::llenar(int fil, int indice) {
-    if (fil == 1)
-        return 1;
-    if (indice == 0 || indice == fil - 1)
-        return 1;
-    else
-        return llenar(fil - 1, indice - 1) + llenar(fil - 1, indice);
+int Operaciones::factorial(int n)
+{
+    int f = 1;
+    for (int i = 1; i <= n; i++)
+        f = f*i;
+    return f;
 }
 
-void Operaciones::espacio(int n) {
-    if (n > 0) {
-        cout << " ";
-        espacio(n - 1);
-    }
-    else cout << "";
+int Operaciones::combinacion(int n, int k)
+{
+    return (factorial(n)/(factorial(k)*(factorial(n - k))));
 }
-void Operaciones::imprimir(int pos, int fil,int n, int esp, bool band) {
 
-    if (fil <= n) {
-        if (band == true) {
-            espacio(esp);
-            band = false;
-        }
-        cout << llenar(fil, pos) << " ";
-        if (pos < fil - 1)
-            imprimir(pos + 1, fil, n, esp, band);
-        else {
-            cout << endl;
-            band = true;
-            imprimir(0, fil + 1, n, esp - 1, band);
-        }
-    }
-    else {
-        cout << endl;
-        band = true;
-    }
-}
-void Operaciones::triangulo(int fill){
-    imprimir(0, 1, fill,fill, true);
-}
