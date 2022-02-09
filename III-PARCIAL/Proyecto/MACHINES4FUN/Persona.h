3e9ef840432e4b338b7f546d8c98055d073aee41
alvarozumba2 <57681168+alvarozumba2@users.noreply.github.com>
2022-02-08 19:46:50 -0500

    Add files via upload

---
+++ III-PARCIAL/Proyecto/MACHINES4FUN/Persona.h
@@ -0,0 +1,33 @@
+/***********************************************************************
+ * Module:  Persona.h
+ * Author:  Alvaro Zuma, Johnny Loachamin
+ * Modified: Tuesday, February 08, 2021 19:25:30 PM
+ * Purpose: Declaration of the class Persona
+ ***********************************************************************/
+#include<iostream>
+
+class Persona
+{
+public:
+   Persona();
+   ~Persona();
+   std::string getId(void);
+   void setId(std::string newId);
+   std::string getNombre(void);
+   void setNombre(std::string newNombre);
+   std::string getApellido(void);
+   void setApellido(std::string newApellido);
+   std::string getCorreo(void);
+   void setCorreo(std::string newCorreo);
+
+protected:
+private:
+   std::string id;
+   std::string nombre;
+   std::string apellido;
+   std::string correo;
+
+
+};
+
+
